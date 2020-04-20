d7866f51dd2eea2503b129fa28eae0e8e02da09e
Maksym Dudkin <maksym@clearmove.com>
2020-04-20 21:21:26 +0300

    test: covered fast-style with tests
    Add validation for sizes enum
    Added test for combine numeric enums

--- packages/fast-style/src/font.ts
+++ packages/fast-style/src/font.ts
@@ -1,7 +1,12 @@
 import { combine, Enum } from '@rnw-community/object-field-tree';
 
-export const getFont = (fontFamilyObj: Enum, fontSizeObj: Enum, fontColorObj: Enum) =>
-    combine(
+export const getFont = (fontFamilyObj: Enum, fontSizeObj: Enum<string>, fontColorObj: Enum) => {
+    // noinspection SuspiciousTypeOfGuard
+    if (Object.values(fontSizeObj).some((item) => typeof item === 'number')) {
+        throw new Error('fontSizeObj must have string values');
+    }
+
+    return combine(
         (fontFamily, fontSize, color) => ({
             fontFamily: fontFamilyObj[fontFamily],
             fontSize: parseInt(fontSizeObj[fontSize], 10),
@@ -11,3 +16,4 @@ export const getFont = (fontFamilyObj: Enum, fontSizeObj: Enum, fontColorObj: En
         fontSizeObj,
         fontColorObj
     );
+};
