499234fdfe8eee9e5ca48789131703ce422726ee
Maksym Dudkin <maksym@clearmove.com>
2020-04-27 11:11:21 +0300

    refactor: extract components and made index.ts just a file with exports

---
+++ packages/hoverable/src/component/hover-view.ts
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+import { withHover } from '@hoverable/with-hover.hoc';
+
+export const HoverView = withHover(View);
--- packages/hoverable/src/index.ts
+++ packages/hoverable/src/index.ts
@@ -1,14 +1,11 @@
-import { Image, ScrollView, Text, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
-
-import { withHover } from './with-hover.hoc';
-
-export const HoverText = withHover(Text);
-export const HoverView = withHover(View);
-export const HoverImage = withHover(Image);
-export const HoverScrollView = withHover(ScrollView);
-export const HoverTouchableOpacity = withHover(TouchableOpacity);
-export const HoverTouchableHighlight = withHover(TouchableHighlight);
-export const HoverTouchableWithoutFeedback = withHover(TouchableWithoutFeedback);
-
 export { HoverProps } from './hover.props';
-export { withHover };
+
+export { withHover } from './with-hover.hoc';
+
+export { HoverImage } from './component/hover-image';
+export { HoverText } from './component/hover-text';
+export { HoverScrollView } from './component/hover-scroll-view';
+export { HoverTouchableHighlight } from './component/hover-touchable-highlight';
+export { HoverTouchableOpacity } from './component/hover-touchable-opacity';
+export { HoverTouchableWithoutFeedback } from './component/hover-touchable-without-feedback';
+export { HoverView } from './component/hover-view';
