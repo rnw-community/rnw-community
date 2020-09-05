1591a7ada80ee42fed183ddb6bec4c211d81eeb8
Vitalii Yehorov <vitalii@clearmove.com>
2020-09-05 09:02:22 +0300

    feat(platform): deprecated storage, updated deps

--- packages/platform/src/storage/storage.ts
+++ packages/platform/src/storage/storage.ts
@@ -1,3 +1,4 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
+/** @deprecated Use @react-native-community/async-storage, it has web fallback support */
 export const storage = AsyncStorage;
--- packages/platform/src/storage/storage.web.ts
+++ packages/platform/src/storage/storage.web.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
 import webStorage from 'redux-persist/lib/storage';
 
+/** @deprecated Use @react-native-community/async-storage, it has web fallback support */
 export const storage = webStorage;
