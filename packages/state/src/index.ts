289f34efade749f9e3d94c497e77191bb8caaf7d
Vitalii Yehorov <vitalii@clearmove.com>
2021-03-16 08:33:15 +0200

    feat(state): added generic epic

--- packages/state/src/index.ts
+++ packages/state/src/index.ts
@@ -1,10 +1,11 @@
 export * from './request-state.interface';
 export * from './request-actions';
 export * from './request-reducers';
-export * from './create-request-state';
 
 export * from './epic/request-epic';
 export * from './epic/switch-epic';
 export * from './epic/concat-epic';
 export * from './epic/exhaust-epic';
 export * from './epic/subscription-epic';
+
+export * from './create-request-state';
