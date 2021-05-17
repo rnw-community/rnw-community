582a248e6744491500814a8c1f1d7ead33054bb5
Vitalii Yehorov <vitalii@clearmove.com>
2021-05-17 23:31:43 +0300

    fix(shared): fixed OnEventFn type

--- packages/shared/src/type/on-event-fn.type.ts
+++ packages/shared/src/type/on-event-fn.type.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+// eslint-disable-next-line @typescript-eslint/no-invalid-void-type, @typescript-eslint/no-explicit-any
 export type OnEventFn<T = Record<string, unknown>, R = void> = (event: T) => R;
