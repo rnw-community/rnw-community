94adc7fd903ca06109f90b1ff63d78532b177e9e
Vitalii Yehorov <vitalii@dotgo.club>
2022-07-26 13:33:06 +0200

    fix(wdio): fixed slow input command signature

--- packages/wdio/src/wdio.d.ts
+++ packages/wdio/src/wdio.d.ts
@@ -16,7 +16,7 @@ declare namespace WebdriverIO {
     interface Element {
         clearInput: () => Promise<void>;
         relativeClick: (xPercent: number, yPercent: number) => Promise<void>;
-        slowInput: (value: string, delay: number) => Promise<void>;
+        slowInput: (value: string, delay?: number) => Promise<void>;
         swipe: (direction: SwipeDirectionType, offset = { x: 0, y: 0 }) => Promise<void>;
         testID$: (testID: string) => Promise<Element>;
         testID$$: (testID: string) => Promise<ElementArray>;
