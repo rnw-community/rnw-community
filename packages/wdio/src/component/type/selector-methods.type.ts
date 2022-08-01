c494d17050a1a4e5ac09c06f6dda5b9be1cda432
Vitalii Yehorov <vitalii@dotgo.club>
2022-08-01 17:27:03 +0200

    fix(wdio): fixed selectorMethods type

--- packages/wdio/src/component/type/selector-methods.type.ts
+++ packages/wdio/src/component/type/selector-methods.type.ts
@@ -1,3 +1,1 @@
-// @ts-expect-error We ignore prefix, how we can avoid TS error?
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export type SelectorMethods<T extends string> = T extends `${infer Prefix}.${infer Selector}` ? `${Selector}` : never;
+export type SelectorMethods<T extends string> = T extends `${string}.${infer Selector}` ? `${Selector}` : never;
