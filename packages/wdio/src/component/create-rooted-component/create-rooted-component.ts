9cc07a0f87e5e0a4850df54c932f42184546ebd3
Vitalii Yehorov <vitalii@dotgo.club>
2022-08-02 14:27:06 +0200

    fix(wdio): fixed createRootedComponent return type

--- packages/wdio/src/component/create-rooted-component/create-rooted-component.ts
+++ packages/wdio/src/component/create-rooted-component/create-rooted-component.ts
@@ -1,9 +1,9 @@
 import { getRootedComponent } from '../get-rooted-component/get-rooted-component';
 
 import type { Enum } from '../../type';
-import type { ComponentWithSelectors } from '../type';
+import type { RootedComponentWithSelectors } from '../type';
 
 export const createRootedComponent = <T extends string>(
     selectors: Enum<T>,
     selectorOrElement?: WebdriverIO.Element | string
-): ComponentWithSelectors<T> => new (getRootedComponent<T>(selectors))(selectorOrElement);
+): RootedComponentWithSelectors<T> => new (getRootedComponent<T>(selectors))(selectorOrElement);
