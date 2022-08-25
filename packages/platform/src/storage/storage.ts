9193abcfba96e97d2dec3722b0a3e0c76fe9c08f
Vitalii Yehorov <vitalii@dotgo.club>
2022-08-25 14:06:57 +0200

    feat(platform): migrated async storage to @react-native-async-storage/async-storage

--- packages/platform/src/storage/storage.ts
+++ packages/platform/src/storage/storage.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /** @deprecated Use @react-native-community/async-storage, it has web fallback support */
 export const storage = AsyncStorage;
