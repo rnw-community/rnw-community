48108eddd0ca1767362de6e486f365cc23fb85cc
Vitalii Yehorov <vitalii@dotgo.club>
2022-09-27 13:06:40 +0200

    fix(wdio): fixed SelectorObject types to support ChainablePromiseElement/Array

--- packages/wdio/src/component/index.ts
+++ packages/wdio/src/component/index.ts
@@ -1,12 +1,2 @@
-export { Component } from './component/component';
-export { getExtendedComponent } from './component/get-exteded-component/get-extended-component';
-export { getComponent } from './component/get-component/get-component';
-export { createComponent } from './component/create-component/create-component';
-export { create$Component } from './component/create$-component/create$-component';
-export { get$Component } from './component/get$-component/get$-component';
-
-export { RootedComponent } from './rooted-component/rooted-component';
-export { getRootedComponent } from './rooted-component/get-rooted-component/get-rooted-component';
-export { createRootedComponent } from './rooted-component/create-rooted-component/create-rooted-component';
-export { create$RootedComponent } from './rooted-component/create$-rooted-component/create$-rooted-component';
-export { get$RootedComponent } from './rooted-component/get$-rooted-component/get$-rooted-component';
+export * from './component';
+export * from './rooted-component';
---
+++ packages/wdio/src/component/rooted-component/index.ts
@@ -0,0 +1,6 @@
+export { RootedComponent } from './rooted-component';
+export { getExtendedRootedComponent } from './get-extended-rooted-component/get-extended-rooted-component';
+export { getRootedComponent } from './get-rooted-component/get-rooted-component';
+export { get$RootedComponent } from './get$-rooted-component/get$-rooted-component';
+export { createRootedComponent } from './create-rooted-component/create-rooted-component';
+export { create$RootedComponent } from './create$-rooted-component/create$-rooted-component';
