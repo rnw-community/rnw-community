55b892539c83055ddc323e522b25071d0243845e
Vitalii Yehorov <vitalii@dotgo.club>
2022-09-29 20:49:02 +0200

    fix(wdio): fixed byIdx and Element as RootEl in constructor

--- packages/wdio/src/command/by-index.command.ts
+++ packages/wdio/src/command/by-index.command.ts
@@ -4,7 +4,7 @@ import type { ChainablePromiseElement } from 'webdriverio';
 export const byIndex$$: ElsIndexSelectorFn = (testID, index, context = browser) =>
     context.$$(testID).then(elements => {
         if (index >= elements.length || index < 0) {
-            throw new Error(`Cannot get item by testID "${testID}" with index "${index}"`);
+            throw new Error(`Cannot get item by $$ "${testID}" with index "${index}"`);
         }
 
         return $(elements[index]);
