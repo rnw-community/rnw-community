1fa9ff05119d825b5adc2e49becacb6510409a86
Vitalii Yehorov <vitalii@dotgo.club>
2022-10-02 13:09:54 +0200

    fix(wdio): fixed byIndex commands

--- packages/wdio/src/command/by-index-command/by-index.command.spec.ts
+++ packages/wdio/src/command/by-index-command/by-index.command.spec.ts
@@ -12,13 +12,4 @@ describe('byIndex$$', () => {
         expect(browser.$$).toHaveBeenNthCalledWith(1, selector);
         expect($).toHaveBeenNthCalledWith(1, { 'element-6066-11e4-a52e-4f735466cecf': mockWdioElement.elementId });
     });
-
-    it('should throw error if wdio elements does not have index', async () => {
-        expect.assertions(2);
-
-        const selector = 'test-selector';
-
-        await expect(byIndex$$(selector, 1)).rejects.toThrow(`Cannot get item by $$ "${selector}" with index "1"`);
-        await expect(byIndex$$(selector, -1)).rejects.toThrow(`Cannot get item by $$ "${selector}" with index "-1"`);
-    });
 });
