0394bd97a926e6c8f8a0ee252d15e21c1e17f209
Vitalii Yehorov <vitalii@dotgo.club>
2022-10-11 19:06:16 +0200

    fix(wdio): fixed RootedComponent new types

--- packages/wdio/src/component/type/rooted-component-with-selectors.type.ts
+++ packages/wdio/src/component/type/rooted-component-with-selectors.type.ts
@@ -2,4 +2,5 @@ import type { RootedComponent } from '../rooted-component/rooted-component';
 import type { SelectorElement } from '../selector-element/selector-element';
 
 export type RootedComponentWithSelectors<T> = Omit<Record<keyof T, SelectorElement & WebdriverIO.Element>, 'Root'> &
-    RootedComponent<T>;
+    RootedComponent<T> &
+    WebdriverIO.Element;
