b02f54f5b6dda1a68cb156a7c006a5e88da76af3
Vitaly Iegorov <vitalyiegorov@gmail.com>
2022-10-20 12:30:16 +0200

    feat(wdio): added getComponentFromEls (#123)
    * feat(wdio): added getComponentFromEls
    
    * feat(wdio): added getComponentFromEls
    
    Co-authored-by: Vitalii Yehorov <vitalii@dotgo.club>

--- packages/wdio/src/component/componnet.spec.ts
+++ packages/wdio/src/component/componnet.spec.ts
@@ -1,4 +1,4 @@
-import { mockDefaultConfig } from '../element.mock';
+import { mockDefaultConfig, mockElement } from '../element.mock';
 
 import { Component } from './component';
 import { ComponentSelectorsMock } from './mocks/component-selectors.mock';
@@ -20,5 +20,37 @@ describe('Component', () => {
 
         await component.getChildEls('test-selector');
         expect(mockDefaultConfig.elsSelectorFn).toHaveBeenCalledWith('test-selector');
+    });
+
+    it('should find component from array of elements by using getComponentFromEls', async () => {
+        expect.assertions(1);
+
+        const component = new Component(mockDefaultConfig, ComponentSelectorsMock);
+
+        const componentFn = jest.fn();
+        const predicateFn = jest.fn();
+
+        componentFn.mockImplementation(() => Promise.resolve(mockElement));
+        predicateFn.mockImplementation(() => true);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const result = await component.getComponentFromEls(ComponentSelectorsMock.Button, componentFn, predicateFn);
+
+        expect(result).toBe(mockElement);
+    });
+
+    it('should throw error component is not found using getComponentFromEls', async () => {
+        expect.assertions(1);
+
+        const component = new Component(mockDefaultConfig, ComponentSelectorsMock);
+
+        const componentFn = jest.fn();
+        const predicateFn = jest.fn();
+
+        componentFn.mockImplementation(() => Promise.resolve(mockElement));
+        predicateFn.mockImplementation(() => false);
+
+        await expect(component.getComponentFromEls(ComponentSelectorsMock.Button, componentFn, predicateFn)).rejects.toThrow(
+            `Failed finding component from elements array "${ComponentSelectorsMock.Button}"`
+        );
     });
 });
