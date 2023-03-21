b61e2d03f3af7b5998e605f6946a2bb05bef7a44
Maksym Dudkin <lugamax@gmail.com>
2023-03-21 20:04:32 +0200

    fix: remove usage of Maybe

--- packages/shared/src/util/get-defined-async/get-defined-async.ts
+++ packages/shared/src/util/get-defined-async/get-defined-async.ts
@@ -1,6 +1,4 @@
 import { isDefined } from '../../type-guard/is-defined/is-defined';
 
-import type { Maybe } from '../../type/maybe.type';
-
-export const getDefinedAsync = <T>(value: Maybe<T>, defaultFn: () => Promise<T>): Promise<T> =>
+export const getDefinedAsync = <T>(value: T | null | undefined, defaultFn: () => Promise<T>): Promise<T> =>
     isDefined(value) ? Promise.resolve(value) : defaultFn();
