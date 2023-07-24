1f6618e00ef99510d317be4faa1c87d4ea34e40d
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-07-24 08:53:04 +0200

    chore: fixed static code checks

--- packages/react-native-payments/android/src/main/java/com/payments/PaymentsModule.java
+++ packages/react-native-payments/android/src/main/java/com/payments/PaymentsModule.java
@@ -1,45 +1,46 @@
 package com.payments;
 
-import androidx.annotation.NonNull;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wallet.*;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.BaseActivityEventListener;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
 
 // HINT: To see crashes use: `adb logcat "*:S" AndroidRuntime:E`
-// HINT: Check this setup: https://developers.google.com/pay/api/android/guides/setup Add it to the docs
+// TODO: Remove event emitter and use main promise as this would be more then enough
 public class PaymentsModule extends PaymentsSpec {
     public static final String NAME = "Payments";
-
+    private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
+    // TODO: Improve this error
     private static final String E_FAILED_SHOWING_ANDROID_PAY = "E_FAILED_SHOWING_ANDROID_PAY";
-    private static final String E_UNSUPPORTED_ANDROID_PAY = "E_UNSUPPORTED_ANDROID_PAY";
-    private static final String E_FAILED_CREATING_PAYMENT_REQUEST = "E_FAILED_CREATING_PAYMENT_REQUEST";
+    private static final String E_ABORTED = "E_ABORTED";
+    private static final String E_FAILED_ABORTING = "E_FAILED_ABORTING";
+    private static final String E_CANCELLED_BY_USER = "E_CANCELLED_BY_USER";
     private static final String E_FAILED_PROCESSING = "E_FAILED_PROCESSING";
     private static final String E_FAILED_UNHANDLED = "E_FAILED_UNHANDLED";
-    private static final String E_FAILED_PARSING_PAYMENT_REQUEST = "E_FAILED_PARSING_PAYMENT_REQUEST";
-    private static final String E_FAILED_PARSING_PAYMENT_RESPONSE = "E_FAILED_PARSING_PAYMENT_RESPONSE";
-    private static final String E_CANCELLED_BY_USER = "E_CANCELLED_BY_USER";
 
-    // Arbitrarily-picked constant integer you define to track a request for payment data activity.
+    // TODO: Find google docs href for this
     private static final int LOAD_MASKED_WALLET_REQUEST_CODE = 88;
 
+    // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient
+    private PaymentsClient mPaymentsClient;
     // TODO: Promise ref, should resolve/reject when the payment is done/cancelled/error
     private Promise mPromise;
-
     // https://reactnative.dev/docs/native-modules-android#getting-activity-result-from-startactivityforresult
     private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
         @Override
@@ -49,28 +50,32 @@ public class PaymentsModule extends PaymentsSpec {
 
             // value passed in AutoResolveHelper
             switch (requestCode) {
-              case LOAD_MASKED_WALLET_REQUEST_CODE:
-                  switch (resultCode) {
-                      case Activity.RESULT_OK:
-                          handlePaymentSuccess(PaymentData.getFromIntent(intent));
-                          break;
+                case LOAD_MASKED_WALLET_REQUEST_CODE:
+                    switch (resultCode) {
+                        case Activity.RESULT_OK:
+                            PaymentData paymentData = PaymentData.getFromIntent(intent);
+                            handlePaymentSuccess(paymentData);
+                            break;
 
-                      case Activity.RESULT_CANCELED:
-                          rejectPromise(E_CANCELLED_BY_USER, "User closed AndroidPay without completing the payment");
-                          break;
+                        case Activity.RESULT_CANCELED:
+                            mPromise.reject(E_CANCELLED_BY_USER, "User closed AndroidPay without completing the payment.");
+                            mPromise = null;
+                            break;
 
-                      case AutoResolveHelper.RESULT_ERROR:
-                          Status status = AutoResolveHelper.getStatusFromIntent(intent);
-                          handleError(status.getStatusCode());
-                          break;
-                  }
+                        case AutoResolveHelper.RESULT_ERROR:
+                            Status status = AutoResolveHelper.getStatusFromIntent(intent);
+                            handleError(status.getStatusCode());
+                            break;
+                    }
 
                 default:
-                    rejectPromise(E_FAILED_UNHANDLED, "Unhandled AndroidPay resultCode: " + resultCode);
+                    Log.d(NAME, "Unhandled AndroidPay resultCode: " + resultCode);
+                    mPromise.reject(E_FAILED_UNHANDLED, "Unhandled AndroidPay resultCode: " + resultCode);
+                    mPromise = null;
                     break;
             }
         }
-      };
+    };
 
     PaymentsModule(ReactApplicationContext context) {
         super(context);
@@ -86,85 +91,40 @@ public class PaymentsModule extends PaymentsSpec {
 
     // Implementation See https://reactnative.dev/docs/native-modules-android
 
-    // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient#isReadyToPay(com.google.android.gms.wallet.IsReadyToPayRequest)
+    // TODO: canMakePayment https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient#isReadyToPay(com.google.android.gms.wallet.IsReadyToPayRequest)
     // https://developers.google.com/pay/api/android/guides/tutorial#java
-    @ReactMethod
-    public void canMakePayment(String paymentMethodData, ReadableMap details, final Promise promise) {
-        Log.d(NAME, "Checking if AndroidPay is available " + currentActivity.toString() + details.toString());
-
-        validatePaymentRequestJSON(paymentMethodData);
-
-        try {
-            // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentDataRequest#fromJson(java.lang.String)
-            PaymentDataRequest request = PaymentDataRequest.fromJson(paymentMethodData);
-
-            if(request == null) {
-                rejectPromise(E_UNSUPPORTED_ANDROID_PAY, "AndroidPay is not supported");
-                return;
-            }
-
-            // https://developers.google.com/android/reference/com/google/android/gms/wallet/Wallet.WalletOptions.Builder#setEnvironment(int)
-            Wallet.WalletOptions options = new Wallet.WalletOptions.Builder().setEnvironment(getEnvironment(details)).build();
-
-            // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient
-            PaymentsClient paymentsClient = Wallet.getPaymentsClient(currentActivity, options);
-
-            Task<Boolean> task = paymentsClient.isReadyToPay(request);
-
-            task.addOnCompleteListener(
-                new OnCompleteListener<Boolean>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Boolean> task) {
-                        try {
-                            boolean result = task.getResult(ApiException.class);
-                            if (result) {
-                                resolve(true);
-                            }
-                        } catch (ApiException e) {
-                            reject(false, e);
-                        }
-                    }
-                });
-        } catch (JSONException e) {
-            rejectPromise(E_FAILED_PARSING_PAYMENT_REQUEST, "Failed parsing PaymentRequest JSON string");
-            return;
-        }
-    }
 
     @ReactMethod
     public void show(String paymentMethodData, ReadableMap details, final Promise promise) {
         Activity currentActivity = getCurrentActivity();
-        Log.d(NAME, "Showing AndroidPay " + currentActivity.toString() + details.toString());
+        Log.d(NAME, "Showing AndroidPay " + currentActivity.toString());
+
+
+        if (currentActivity == null) {
+            promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+            return;
+        }
 
         // HINT: We store promise reference to resolve/reject it later
         mPromise = promise;
 
-        validatePaymentRequestJSON(paymentMethodData);
-
         try {
+            int environment = details.hasKey("environment") && details.getString("environment").equals("TEST") ? WalletConstants.ENVIRONMENT_TEST : WalletConstants.ENVIRONMENT_PRODUCTION;
+
             // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentDataRequest#fromJson(java.lang.String)
             PaymentDataRequest request = PaymentDataRequest.fromJson(paymentMethodData);
 
-            if(request == null) {
-                rejectPromise(E_FAILED_CREATING_PAYMENT_REQUEST, "Failed creating PaymentDataRequest");
-                return;
-            }
-
-            Log.d(NAME, "Created PaymentDataRequest " + request.toString());
-
             // https://developers.google.com/android/reference/com/google/android/gms/wallet/Wallet.WalletOptions.Builder#setEnvironment(int)
-            Wallet.WalletOptions options = new Wallet.WalletOptions.Builder().setEnvironment(getEnvironment(details)).build();
-
-            // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient
-            PaymentsClient paymentsClient = Wallet.getPaymentsClient(currentActivity, options);
+            Wallet.WalletOptions options = new Wallet.WalletOptions.Builder().setEnvironment(environment).build();
+            mPaymentsClient = Wallet.getPaymentsClient(currentActivity, options);
 
             // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient#public-taskpaymentdata-loadpaymentdata-paymentdatarequest-request
-            Task<PaymentData> loadPaymentDataTask = paymentsClient.loadPaymentData(request);
-
+            Task<PaymentData> loadPaymentDataTask = mPaymentsClient.loadPaymentData(request);
             // https://developers.google.com/android/reference/com/google/android/gms/wallet/AutoResolveHelper
             AutoResolveHelper.resolveTask(loadPaymentDataTask, currentActivity, LOAD_MASKED_WALLET_REQUEST_CODE);
-        } catch(Exception e) {
-            rejectPromise(E_FAILED_SHOWING_ANDROID_PAY, "Failed showing AndroidPay" + e);
+        } catch (Exception e) {
+            mPromise.reject(E_FAILED_SHOWING_ANDROID_PAY, "Failed showing AndroidPay: " + e);
+            mPromise = null;
         }
     }
 
@@ -172,14 +132,7 @@ public class PaymentsModule extends PaymentsSpec {
     public void abort(Promise promise) {
         Log.d(NAME, "Aborting AndroidPay for " + getCurrentActivity().toString());
 
-        promise.resolve("AndroidPay abort is not supported");
-    }
-
-    @ReactMethod
-    public void complete(String status, Promise promise) {
-        Log.d(NAME, "Completing status " + status + " AndroidPay for " + getCurrentActivity().toString());
-
-        promise.resolve("AndroidPay complete is not supported");
+        promise.reject(E_FAILED_ABORTING, "AndroidPay abort is not supported");
     }
 
     /**
@@ -192,14 +145,17 @@ public class PaymentsModule extends PaymentsSpec {
      */
     private void handlePaymentSuccess(PaymentData paymentData) {
         // https://developers.google.com/pay/api/android/guides/tutorial#checkoutactivity.java-java
-        // https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentData
         final String paymentInfo = paymentData.toJson();
         if (paymentInfo == null) {
-            rejectPromise(E_FAILED_PARSING_PAYMENT_RESPONSE, "Failed parsing payment response");
+            Log.e(NAME, "PaymentDataRequest was not constructed using fromJson(String).");
+
+            mPromise.reject("PaymentDataRequest was not constructed using fromJson(String)");
+            mPromise = null;
+
             return;
         }
 
-        Log.d(NAME, "Successfully received paymentData: " + getCurrentActivity().toString() + " " + paymentData);
+        Log.d(NAME, "Successfully received paymentData" + getCurrentActivity().toString() + " " + paymentData);
 
         mPromise.resolve(paymentInfo);
         mPromise = null;
@@ -215,6 +171,8 @@ public class PaymentsModule extends PaymentsSpec {
      * WalletConstants#constant-summary">Wallet Constants Library</a>
      */
     private void handleError(int statusCode) {
+        Log.e("loadPaymentData failed", String.format("Error code: %d", statusCode));
+
         String errorMessage;
 
         switch (statusCode) {
@@ -233,32 +191,7 @@ public class PaymentsModule extends PaymentsSpec {
                 break;
         }
 
-        rejectPromise(E_FAILED_PROCESSING, errorMessage);
-    }
-
-    private void validatePaymentRequestJSON(String paymentRequestJSON){
-        try {
-            JSONObject jsonObject = new JSONObject(paymentRequestJSON);
-            Log.d(NAME, "Successfully validated paymentRequest JSON string " + jsonObject.toString());
-        } catch (JSONException e) {
-            rejectPromise(E_FAILED_PARSING_PAYMENT_REQUEST, "Failed parsing PaymentRequest JSON string");
-        }
-    }
-
-    private int getEnvironment(ReadableMap details) {
-        if (details.hasKey("environment") && details.getString("environment").equals("PRODUCTION")) {
-            return WalletConstants.ENVIRONMENT_PRODUCTION;
-        }
-
-        return WalletConstants.ENVIRONMENT_TEST;
-    }
-
-    private void rejectPromise(String code, String message) {
-        Log.e(NAME, message);
-
-        if (mPromise != null) {
-            mPromise.reject(code, message);
-            mPromise = null;
-        }
+        mPromise.reject(E_FAILED_PROCESSING, errorMessage);
+        mPromise = null;
     }
 }
