8a6b9f21948b11566a7f70b2cd72741ab17a9495
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-10-22 16:43:50 +0200

    ci: optimizing release-it config

--- .release-it.js
+++ .release-it.js
@@ -3,11 +3,8 @@ const version = '${version}';
 module.exports = {
     plugins: {
         '@release-it/conventional-changelog': {
-            path: '.',
-            infile: 'CHANGELOG.md',
-            preset: 'conventionalcommits',
-            gitRawCommitsOpts: {
-                path: '.',
+            preset: {
+                name: 'conventionalcommits',
             },
         },
         '@release-it-plugins/workspaces': true,
