e15c498010140a2dbf929f4b3f58091e35e0e29a
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-10-26 07:01:05 +0200

    chore: fixed lint

--- packages/shared/src/type-guard/array/is-not-empty-array/is-not-empty-array.ts
+++ packages/shared/src/type-guard/array/is-not-empty-array/is-not-empty-array.ts
@@ -1,5 +1,6 @@
 import { isDefined } from '../../generic/is-defined/is-defined';
-import { IsNotEmptyArray } from '../../../type/is-not-empty-array-type/is-not-empty-array.type';
+
+import type { IsNotEmptyArray } from '../../../type/is-not-empty-array-type/is-not-empty-array.type';
 
 export const isNotEmptyArray = <T>(array: unknown): array is IsNotEmptyArray<T> =>
     isDefined(array) && Array.isArray(array) && array.length > 0;
