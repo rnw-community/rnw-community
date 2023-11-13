1f71ab125e607aac05dc6b6f5f4477669b5d723d
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-11-13 09:54:18 +0100

    fix(shared): improved isNotEmptyArray types

--- packages/shared/src/type-guard/array/is-not-empty-array/is-not-empty-array.ts
+++ packages/shared/src/type-guard/array/is-not-empty-array/is-not-empty-array.ts
@@ -2,5 +2,5 @@ import { isDefined } from '../../generic/is-defined/is-defined';
 
 import type { IsNotEmptyArray } from '../../../type/is-not-empty-array-type/is-not-empty-array.type';
 
-export const isNotEmptyArray = <T>(array: unknown): array is IsNotEmptyArray<T> =>
+export const isNotEmptyArray = <T>(array: T[] | null | undefined): array is IsNotEmptyArray<T> =>
     isDefined(array) && Array.isArray(array) && array.length > 0;
