419d366472530709eda971b504fc1ea8243b81aa
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-12-16 08:33:54 +0100

    feat: added react-eslint-plugin

---
+++ packages/react-eslint-plugin/src/rules/no-complex-jsx-logic/no-complex-jsx-logic.rule.ts
@@ -0,0 +1,49 @@
+import { AST_NODE_TYPES, ESLintUtils } from '@typescript-eslint/utils';
+
+// TODO: Add url?
+const createRule = ESLintUtils.RuleCreator(() => `https://github.com/rnw-community/rnw-community/react-eslint-plugin`);
+
+const noComplexJsxLogic = createRule({
+    name: 'no-complex-jsx-logic',
+    meta: {
+        docs: {
+            description: 'Prevent complex logic in JSX',
+        },
+        messages: {
+            noInlineCallbacks: 'Inline callbacks should not be used inside JSX; they are created on each render.',
+            noTernaryCallbackArguments: 'Conditional expressions should not be used in callback arguments in JSX.',
+            noPropsCalculations: 'Complex logic (calculations) should not be used in JSX props.',
+            noPropsTernary: 'Complex logic (ternary or logical operators) should not be used in JSX props.',
+        },
+        type: 'suggestion',
+        schema: [],
+    },
+    defaultOptions: [],
+    create(context) {
+        return {
+            JSXExpressionContainer(node) {
+                const { expression } = node;
+
+                if (expression.type === AST_NODE_TYPES.ArrowFunctionExpression) {
+                    context.report({ node, messageId: 'noInlineCallbacks' });
+                } else if (node.parent?.type === AST_NODE_TYPES.JSXAttribute) {
+                    if (expression.type === AST_NODE_TYPES.BinaryExpression) {
+                        context.report({ node, messageId: 'noPropsCalculations' });
+                    } else if (expression.type === AST_NODE_TYPES.ConditionalExpression) {
+                        context.report({ node, messageId: 'noPropsTernary' });
+                    } else if (expression.type === AST_NODE_TYPES.LogicalExpression) {
+                        context.report({ node, messageId: 'noPropsTernary' });
+                    } else if (expression.type === AST_NODE_TYPES.CallExpression) {
+                        expression.arguments.forEach(arg => {
+                            if (arg.type === 'ConditionalExpression') {
+                                context.report({ node, messageId: 'noTernaryCallbackArguments' });
+                            }
+                        });
+                    }
+                }
+            },
+        };
+    },
+});
+
+export default noComplexJsxLogic;
