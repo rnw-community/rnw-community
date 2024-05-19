34b563e077759cde94470adedbd2d4e04f3760d0
Vitalii Yehorov <vitalyiegorov@gmail.com>
2024-05-19 10:55:26 +0200

    feat(nestjs-enterprice): added adding log tests

--- packages/nestjs-enterprise/src/decorator/log/log-decorator.spec.ts
+++ packages/nestjs-enterprise/src/decorator/log/log-decorator.spec.ts
@@ -2,6 +2,44 @@ import { Observable, of } from 'rxjs';
 
 import { Log } from './log.decorator';
 
+const preLogText = 'testPreLog';
+const postLogText = 'testPostLog';
+const errorLogText = 'testErrorLog';
+
+class TestClass {
+    private readonly field = 1;
+
+    @Log(preLogText, postLogText)
+    testStrings(): number {
+        return this.field;
+    }
+
+    @Log(preLogText, postLogText)
+    testPromiseStrings(): Promise<number> {
+        return Promise.resolve(this.field);
+    }
+
+    @Log(preLogText, postLogText)
+    testObservableStrings(): Observable<number> {
+        return of(this.field);
+    }
+
+    @Log(arg => `${preLogText}-${arg}`, (result, arg) => `${result}-${postLogText}-${arg}`)
+    testFunctions(arg: number): number {
+        return arg + this.field;
+    }
+
+    @Log(preLogText, postLogText, errorLogText)
+    testErrorString(arg: number): number {
+        throw new Error(`error ${arg}`);
+    }
+
+    @Log(preLogText, postLogText, arg => `${errorLogText}-${arg}`)
+    testErrorFunction(arg: number): number {
+        throw new Error(`error ${arg}`);
+    }
+}
+
 describe('LogDecorator', () => {
     it('should output pre/post logs as strings with Plain value returned', () => {
         const instance = new TestClass();
@@ -39,41 +77,3 @@ describe('LogDecorator', () => {
         expect(() => instance.testErrorFunction(2)).toThrow();
     });
 });
-
-class TestClass {
-    private readonly field = 1;
-
-    @Log('testPreLog', 'testPostLog', 'testErrorLog')
-    testStrings(): number {
-        return this.field;
-    }
-
-    @Log('testPreLog', 'testPostLog', 'testErrorLog')
-    testPromiseStrings(): Promise<number> {
-        return Promise.resolve(this.field);
-    }
-
-    @Log('testPreLog', 'testPostLog', 'testErrorLog')
-    testObservableStrings(): Observable<number> {
-        return of(this.field);
-    }
-
-    @Log(arg => `testPreLogFunction-${arg}`, (result, arg) => `${result}-testPreLogFunction-${arg}`)
-    testFunctions(arg: number): number {
-        return arg + this.field;
-    }
-
-    @Log(arg => `testPreLogFunction-${arg}`, (result, arg) => `${result}-testPreLogFunction-${arg}`, `testErrorLogFunction`)
-    testErrorString(arg: number): number {
-        throw new Error(`error ${arg}`);
-    }
-
-    @Log(
-        arg => `testPreLogFunction-${arg}`,
-        (result, arg) => `${result}-testPreLogFunction-${arg}`,
-        arg => `testErrorLogFunction-${arg}`
-    )
-    testErrorFunction(arg: number): number {
-        throw new Error(`error ${arg}`);
-    }
-}
