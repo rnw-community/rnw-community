7d63971283cfed9ac845882dbafc2ebabfcd9205
Vitaly Iegorov <vitalyiegorov@gmail.com>
2024-06-04 12:09:49 +0200

    feat(nestjs-enterprise): added HistogramMetric decorator (#193)
    * feat(nestjs-enterprice): added new package
    
    * feat(nestjs-enterprice): added adding log tests
    
    * feat(nestjs-enterprice): added adding log tests
    
    * feat(nestjs-enterprice): added adding log tests
    
    * feat(nestjs-enterprice): added adding log tests
    
    * feat(nestjs-enterprice): added adding log tests
    
    * feat(nestjs-enterprice): added adding log tests
    
    * docs(nestjs-enterprise): added doc
    
    * docs(nestjs-enterprise): added doc
    
    * test(nestjs-enterprise): fixed coverage
    
    * test(nestjs-enterprise): fixed coverage
    
    * test(nestjs-enterprise): fixed coverage
    
    * test(nestjs-enterprise): fixed coverage
    
    * feat(nestjs-enterprise): added HistogramMetric decorator
    
    * feat(nestjs-enterprise): updated deps
    
    * feat(nestjs-enterprise): updated deps

---
+++ packages/nestjs-enterprise/src/decorator/histogram-metric/histogram-metric-decorator.spec.ts
@@ -0,0 +1,71 @@
+import { describe, expect, it, jest } from '@jest/globals';
+import { Histogram } from 'prom-client';
+
+import { HistogramMetric } from './histogram-metric.decorator';
+
+/* eslint-disable @typescript-eslint/unbound-method,class-methods-use-this,@typescript-eslint/class-methods-use-this */
+class TestClass {
+    @HistogramMetric('test-metric')
+    testMethod(): number {
+        return 0;
+    }
+
+    @HistogramMetric('test-metric', { buckets: [1], help: 'test-help' })
+    testMethodConfiguration(): number {
+        return 0;
+    }
+
+    @HistogramMetric('test-metric')
+    testMethodError(): number {
+        throw new Error('test-error');
+    }
+}
+
+const mockEndTimer = jest.fn();
+jest.mock('prom-client', () => ({
+    Histogram: jest.fn().mockImplementation(() => ({
+        startTimer: jest.fn().mockImplementation(() => mockEndTimer),
+    })),
+}));
+
+describe(`HistogramMetric decorator`, () => {
+    it('should create and run histogram metric', () => {
+        expect.assertions(2);
+
+        const testClass = new TestClass();
+        testClass.testMethod();
+
+        expect(Histogram).toHaveBeenCalledWith({
+            name: 'test-metric',
+            help: 'test-metric',
+        });
+        expect(mockEndTimer).toHaveBeenCalledWith();
+    });
+
+    it('should create and run histogram metric with configuration', () => {
+        expect.assertions(2);
+
+        const testClass = new TestClass();
+        testClass.testMethodConfiguration();
+
+        expect(Histogram).toHaveBeenCalledWith({
+            name: 'test-metric',
+            help: 'test-help',
+            buckets: [1],
+        });
+        expect(mockEndTimer).toHaveBeenCalledWith();
+    });
+
+    it('should create and run histogram metric with method error', () => {
+        expect.assertions(3);
+
+        const testClass = new TestClass();
+        expect(() => testClass.testMethodError()).toThrow('test-error');
+
+        expect(Histogram).toHaveBeenCalledWith({
+            name: 'test-metric',
+            help: 'test-metric',
+        });
+        expect(mockEndTimer).toHaveBeenCalledWith();
+    });
+});
