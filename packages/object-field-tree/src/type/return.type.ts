30c4fe6604e73bb1391aa60fcca785629dde6531
Maksym Dudkin <lugamax@gmail.com>
2024-07-11 14:12:22 +0300

    fix(fast-style): adjust enum generics so it passes ts

--- packages/object-field-tree/src/type/return.type.ts
+++ packages/object-field-tree/src/type/return.type.ts
@@ -1,13 +1,16 @@
 import type { Enum } from '@rnw-community/shared';
 
-export type Return1<T extends Enum, D> = Record<keyof T, D>;
-export type Return2<D, T1 extends Enum, T2 extends Enum> = Record<keyof T1, Return1<T2, D>>;
-export type Return3<D, T1 extends Enum, T2 extends Enum, T3 extends Enum> = Record<keyof T1, Return2<D, T2, T3>>;
-export type Return4<D, T1 extends Enum, T2 extends Enum, T3 extends Enum, T4 extends Enum> = Record<
+export type CombineReturn1<T extends Enum, D> = Record<keyof T, D>;
+export type CombineReturn2<D, T1 extends Enum, T2 extends Enum> = Record<keyof T1, CombineReturn1<T2, D>>;
+export type CombineReturn3<D, T1 extends Enum, T2 extends Enum, T3 extends Enum> = Record<
     keyof T1,
-    Return3<D, T2, T3, T4>
+    CombineReturn2<D, T2, T3>
 >;
-export type Return5<D, T1 extends Enum, T2 extends Enum, T3 extends Enum, T4 extends Enum, T5 extends Enum> = Record<
+export type CombineReturn4<D, T1 extends Enum, T2 extends Enum, T3 extends Enum, T4 extends Enum> = Record<
     keyof T1,
-    Return4<D, T2, T3, T4, T5>
+    CombineReturn3<D, T2, T3, T4>
+>;
+export type CombineReturn5<D, T1 extends Enum, T2 extends Enum, T3 extends Enum, T4 extends Enum, T5 extends Enum> = Record<
+    keyof T1,
+    CombineReturn4<D, T2, T3, T4, T5>
 >;
