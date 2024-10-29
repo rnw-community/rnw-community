d9f2e6fd984b5679f6c690c1370a113a39b9d375
Vitalii Yehorov <vitalyiegorov@gmail.com>
2024-10-29 07:07:33 +0100

    chore: fixed ci

--- packages/nestjs-enterprise/src/decorator/lock/util/is-promise.util.ts
+++ packages/nestjs-enterprise/src/decorator/lock/util/is-promise.util.ts
@@ -1,10 +1,11 @@
 import { isDefined } from '@rnw-community/shared';
 
-// TODO: Move to shared?
+/*
+ * TODO: Move to shared?
+ * HINT: https://promisesaplus.com/#the-promise-resolution-procedure
+ */
 export const isPromise = (value: unknown): value is Promise<unknown> =>
-    Boolean(
-        (typeof value === 'object' || typeof value === 'function') &&
-            isDefined(value) &&
-            'then' in value &&
-            typeof value.then === 'function'
-    );
+    (typeof value === 'object' || typeof value === 'function') &&
+    isDefined(value) &&
+    'then' in value &&
+    typeof value.then === 'function';
