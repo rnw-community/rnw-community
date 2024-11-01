1bc6b9602b1f961e68e494e23bf0c73d75b43070
Vitalii Yehorov <vitalyiegorov@gmail.com>
2024-11-01 11:51:15 +0100

    revert: removed awesome lib

--- packages/awesome-lib/android/src/newarch/AwesomeLibSpec.kt
+++
@@ -1,7 +0,0 @@
-package com.awesomelib
-
-import com.facebook.react.bridge.ReactApplicationContext
-
-abstract class AwesomeLibSpec internal constructor(context: ReactApplicationContext) :
-  NativeAwesomeLibSpec(context) {
-}
