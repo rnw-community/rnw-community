32869b5be783c66f9617a6c85d485a7d9097aeb1
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-02-03 08:09:48 +0100

    feat(react-native-payments): working rn plain example

--- packages/react-native-payments-example/index.js
+++ packages/react-native-payments-example/index.js
@@ -4,6 +4,4 @@ import { App } from '@rnw-community/react-native-payments-example-lib';
 
 import { name as appName } from './app.json';
 
-console.log(App);
-
 AppRegistry.registerComponent(appName, () => App);
