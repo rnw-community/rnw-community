ea8cd7f89904d5f3d23631ec4d088a8a497eb546
Vitaly Iegorov <vitalyiegorov@gmail.com>
2025-03-02 08:54:14 +0100

    fix(react-native-payments):  fix ApplePay closing on redirect to another screen (#275)
    * fix(react-native-payments):  fix ApplePay closing on redirect to another screen, dark theme support
    
    * fix(react-native-payments):  fix ApplePay closing on redirect to another screen, dark theme support

--- packages/react-native-payments-example-lib/src/component/switch-row.tsx
+++ packages/react-native-payments-example-lib/src/component/switch-row.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Text, View, type ViewStyle } from 'react-native';
+import { Switch, Text, View, type ViewStyle, useColorScheme } from 'react-native';
+
+import type { ReactNode } from 'react';
 
 interface SwitchRowProps {
     readonly setValue: (val: boolean) => void;
@@ -12,15 +14,21 @@ const viewStyle: ViewStyle = {
     justifyContent: 'space-between',
 };
 
-export const SwitchRow = ({ text, value, setValue }: SwitchRowProps): React.ReactNode => (
-    <View style={viewStyle}>
-        <Text>{text}</Text>
+export const SwitchRow = ({ text, value, setValue }: SwitchRowProps): ReactNode => {
+    const colorScheme = useColorScheme();
 
-        <Switch
-            ios_backgroundColor="#3e3e3e"
-            onValueChange={setValue}
-            trackColor={{ false: '#767577', true: '#5BC236' }}
-            value={value}
-        />
-    </View>
-);
+    const textStyle = { color: colorScheme === 'dark' ? '#FFFFFF' : '#000000' };
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{text}</Text>
+
+            <Switch
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={setValue}
+                trackColor={{ false: '#767577', true: '#5BC236' }}
+                value={value}
+            />
+        </View>
+    );
+};
