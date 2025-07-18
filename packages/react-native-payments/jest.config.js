f10b109f149423ed06f3636cb4ea44d27f8d6078
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-07-18 10:16:01 +0200

    feat: bump dev deps, added PR name lint job

--- packages/react-native-payments/jest.config.js
+++ packages/react-native-payments/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     coverageThreshold: {
         global: {
             branches: 76.15,
+            lines: 99.9,
+            statements: 99.9,
+            functions: 99.9,
         },
     },
 };
