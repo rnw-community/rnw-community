e60daca4e423962617663c88df49e20f1d549ad0
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-10-24 07:35:04 +0200

    feat(eslint-plugin): added recommended config, added meta

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,12 +1,29 @@
+import pkg from '../package.json';
+
 import { rules } from './rules';
 
 import type { ESLint } from 'eslint';
+
 interface Plugin extends Omit<ESLint.Plugin, 'rules'> {
     rules: typeof rules;
 }
 
 const plugin: Plugin = {
+    meta: {
+        name: pkg.name,
+        version: pkg.version,
+    },
+    configs: {},
     rules,
 };
+
+Object.assign(plugin.configs ?? {}, {
+    recommended: [
+        {
+            plugins: { example: plugin },
+            rules: { 'no-complex-jsx-logic': 'error' },
+        },
+    ],
+});
 
 export = plugin;
