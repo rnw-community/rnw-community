74588272c173ad70b358c64423ba1e5332f8758d
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-08-09 12:22:06 +0200

    chore(redux-loadable): remove non-compliant comments per repo comments policy

--- packages/redux-loadable/src/utils/loading-reset.util.ts
+++ packages/redux-loadable/src/utils/loading-reset.util.ts
@@ -2,14 +2,6 @@ import { initialLoadingState } from '../interface/loading-state.interface.js';
 
 import type { LoadingStateInterface } from '../interface/loading-state.interface.js';
 
-/**
- * Reset loading state slice to loading initial state.
- *
- * @template T
- * @param state Redux state slice
- *
- * @return {T} Mutated state
- */
 export const loadingReset = <T extends LoadingStateInterface>(state: T): T => {
     state.isPristine = initialLoadingState.isPristine;
     state.isLoading = initialLoadingState.isLoading;
--- packages/redux-loadable/src/utils/loading-started.util.ts
+++ packages/redux-loadable/src/utils/loading-started.util.ts
@@ -2,14 +2,6 @@ import { initialLoadingState } from '../interface/loading-state.interface.js';
 
 import type { LoadingStateInterface } from '../interface/loading-state.interface.js';
 
-/**
- * Change loading state slice to loading started state.
- *
- * @template T
- * @param state Redux state slice
- *
- * @return {T} Mutated state
- */
 export const loadingStarted = <T extends LoadingStateInterface>(state: T): T => {
     state.isPristine = false;
     state.isLoading = true;
