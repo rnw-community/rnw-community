df3e54450f5be8793ad9436572d74bcf2e53a21b
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-08-24 06:46:20 +0200

    refactor: centralize pnpm-aware jest transform patterns

--- packages/platform/jest.config.js
+++ packages/platform/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    ...require('../../get-jest.config.js')('platform', '@react-native/jest-preset'),
-    transformIgnorePatterns: [
-        'node_modules/(?!\\.pnpm|((jest-)?react-native|@react-native(-community)?)/)',
-        'node_modules/\\.pnpm/[^/]+/node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)',
-    ],
+    ...require('../../get-jest.config.js')('platform', '@react-native/jest-preset', [
+        '(jest-)?react-native',
+        '@react-native(-community)?',
+    ]),
 };
--- packages/react-native-collapsible-header/jest.config.js
+++ packages/react-native-collapsible-header/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    ...require('../../get-jest.config.js')('react-native-collapsible-header', '@react-native/jest-preset'),
+    ...require('../../get-jest.config.js')('react-native-collapsible-header', '@react-native/jest-preset', [
+        '(jest-)?react-native',
+        '@react-native(-community)?',
+        'react-native-reanimated',
+        'react-native-worklets',
+    ]),
     collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!<rootDir>/src/**/*.spec.{ts,tsx}', '!<rootDir>/src/**/*.d.ts'],
     resolver: 'react-native-worklets/jest/resolver',
     setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-    transformIgnorePatterns: [
-        'node_modules/(?!\\.pnpm|((jest-)?react-native|@react-native(-community)?|react-native-reanimated|react-native-worklets)/)',
-        'node_modules/\\.pnpm/[^/]+/node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-reanimated|react-native-worklets)/)',
-    ],
 };
